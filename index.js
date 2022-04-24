7da7405707d5c89599f45d1d5351144fae7eec0e
Thabata <thabatagcampos@gmail.com>
2022-04-24 20:51:59 -0300

    update

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ let pokedex = [
         name:  'Bulbasaur',
         image: '/css/001.png',
         description: 'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
-        heighy: '0,7 m',
-        weighy: '6,9 kg',
+        height: '0,7 m',
+        weight: '6,9 kg',
         category: 'Seed',
         abilities: 'Overgrow'
     },
@@ -35,8 +35,8 @@ let pokedex = [
         name:  'Pikachu',
         image: '/css/025.png',
         description: 'Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.',
-        heighy: '0,4 m',
-        weighy: '6 kg',
+        height: '0,4 m',
+        weight: '6 kg',
         category: 'Mouse',
         abilities: 'Static'
     },
@@ -46,10 +46,43 @@ let pokedex = [
         name:  'Togepi',
         image: '/css/175.png',
         description: 'The shell seems to be filled with joy. It is said that it will share good luck when treated kindly.',
-        heighy: '0,3 m',
-        weighy: '1,5 kg',
+        height: '0,3 m',
+        weight: '1,5 kg',
         category: 'Spike Ball',
         abilities: 'Serene Grace/Hustle'
+    },
+    {   id: 4,
+        number: '133',
+        type:  'Normal',
+        name:  'Eevee',
+        image: '/css/133.png',
+        description: 'It has the ability to alter the composition of its body to suit its surrounding environment.',
+        height: '0.3 m',
+        weight: '6,5 kg',
+        category: 'Evolution',
+        abilities: 'Run Away/Adaptability'
+    },
+    {   id: 5,
+        number: '152',
+        type:  'Grass',
+        name:  'Chikorita',
+        image: '/css/152.png',
+        description: 'In battle, Chikorita waves its leaf around to keep the foe at bay. However, a sweet fragrance also wafts from the leaf, becalming the battling Pokémon and creating a cozy, friendly atmosphere all around.',
+        height: '0,9 m',
+        weight: '6,4 kg',
+        category: 'Leaf',
+        abilities: 'Overgrow'
+    },
+    {   id: 6,
+        number: '700',
+        type:  'Fairy',
+        name:  'Sylveon',
+        image: '/css/700.png',
+        description: 'By releasing enmity-erasing waves from its ribbonlike feelers, Sylveon stops any conflict.',
+        height: '1,0 m',
+        weight: '23,5 kg',
+        category: 'Intertwining',
+        abilities: 'Cute Charm'
     },
 ]
 
@@ -71,8 +104,8 @@ app.get('/cadastro', (req, res) => {
 })
 app.post('/cadastro', (req, res) => {
     let i = pokedex.length+1
-    const {number, type, name, image, description, heighy, weighy, category, abilities} = req.body
-    let pokemon={id: i, number, type, name, image, description, heighy, weighy, category, abilities}
+    const {number, type, name, image, description, height, weight, category, abilities} = req.body
+    let pokemon={id: i, number, type, name, image, description, height, weight, category, abilities}
     pokedex.push(pokemon)
     res.redirect('/')
 
