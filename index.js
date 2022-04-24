5fb42b32ed5a6605e3f290a283231544a1062c83
Thabata <thabatagcampos@gmail.com>
2022-04-24 17:37:56 -0300

    update

--- index.js
+++ index.js
@@ -17,33 +17,35 @@ app.listen(port, () => {
 })
 
 let pokedex = [
-    {   numero: '001',
+    {   id: 1,
+        numero: '001',
         tipo:  'Grama/Venenoso',
         nome:  'Bulbasaur',
         imagem: '/css/001.png',
-        descrição: 'Em suas costas há um bulbo que é cultivado a partir de uma semente plantada no seu nascimento. O bulbo também esconde duas videiras delgadas e semelhantes a tentáculos e fornece energia através da fotossíntese.',
+        descricao: 'Em suas costas há um bulbo que é cultivado a partir de uma semente plantada no seu nascimento. O bulbo também esconde duas videiras delgadas e semelhantes a tentáculos e fornece energia através da fotossíntese.',
         altura: '0,7 m',
         peso: '6,9 kg',
         categoria: 'Semente',
         habilidade: 'Supercrescimento'
     },
     {
+        id: 2,
         numero: '025',
         tipo:  'Elétrico',
         nome:  'Pikachu',
         imagem: '/css/025.png',
-        descrição: 'Tem a capacidade de gerar eletricidade através de suas bolsas elétricas localizadas em suas bochechas.',
+        descricao: 'Tem a capacidade de gerar eletricidade através de suas bolsas elétricas localizadas em suas bochechas.',
         altura: '0,4 m',
         peso: '6 kg',
         categoria: 'Rato',
         habilidade: 'Estática'
     },
-    {
+    {   id: 3,
         numero: '175',
         tipo:  'Fada',
         nome:  'Togepi',
         imagem: '/css/175.png',
-        descrição: 'É repleto de energia positiva, se for tratado com bondade, trará boa sorte ao seu treinador. É considerado como símbolo de boa sorte',
+        descricao: 'É repleto de energia positiva, se for tratado com bondade, trará boa sorte ao seu treinador. É considerado como símbolo de boa sorte',
         altura: '0,3 m',
         peso: '1,5 kg',
         categoria: 'Bola de Espinho',
@@ -56,24 +58,21 @@ app.get('/', (req, res) => {
 res.render('index.ejs', {pokedex})
 })
 
-app.get('/detalhes/:numero', (req, res) => {
-    let pokemon=[];
-    pokedex.filter((element)=>{
-        if(element.numero==req.params.numero){
-            pokemon[0]=element
-        }
-    });
-    res.render('detalhes.ejs',{
-        pokemon
-    });
-});
+app.get('/detalhes/:id', (req, res) => {
+const id = +req.params.id;
+const pokemon = pokedex.find(pokemon => pokemon.id === id);
+
+res.render("detalhes", {pokemon});
+console.log(pokemon)
+    })
 
 app.get('/cadastro', (req, res) => {
-    res.render('cadastro.ejs');
-});
-    
-app.post('/cadastro/add', (req, res) => {
-    const data = req.body;
-    pokedex.push(data);
-    res.redirect('/');
-});
+    res.render('cadastro.ejs')
+})
+app.post('/cadastro', (req, res) => {
+    let i = pokedex[pokedex.length-1].id + 1
+    const { numero, tipo, nome, imagem, descrição, altura, peso, categoria, habilidade } = req.body
+    pokedex.push({id: i, numero, tipo, nome, imagem, descrição, altura, peso, categoria, habilidade})
+    console.log(pokedex)
+    res.redirect('/')
+})
